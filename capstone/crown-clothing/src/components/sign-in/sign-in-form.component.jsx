21e59e9d05903443eab55cb0c14b124547b81120
Thomas Ryan <DWTrip@gmail.com>
2023-06-27 13:37:50 +0900

    Added sign-in and authentication.

---
+++ capstone/crown-clothing/src/components/sign-in/sign-in-form.component.jsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword,
+} from "../../utils/firebase/firebase.utils";
+
+import Button from "../button/button.component";
+import FormInput from "../form-input/form-input.component";
+
+import "./sign-in-form.styles.scss";
+
+const defaultInputValues = {
+  email: "",
+  password: "",
+};
+
+const SignInForm = () => {
+  const [formInputValues, setFormInputValues] = useState(defaultInputValues);
+
+  const { email, password } = formInputValues;
+
+  const resetFormFields = () => {
+    setFormInputValues(defaultInputValues);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormInputValues({ ...formInputValues, [name]: value });
+  };
+
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(user);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await signInAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(response);
+      resetFormFields();
+    } catch (err) {
+      switch (err.code) {
+        case "auth/wrong-password":
+          alert("Incorrect password for email");
+          break;
+        case "auth/user-not-found":
+          alert("no user associated with this email");
+          break;
+        default:
+          alert(`${err.code}`);
+      }
+    }
+  };
+
+  return (
+    <div className="sign-up-container">
+      <h2>I already have an account</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label={"Email"}
+          type="text"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+        <FormInput
+          label={"Password"}
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+
+        <div className="buttons-container">
+          <Button children={"Sign In"} type="submit" />
+          <Button
+            children={"Sign In With Google"}
+            buttonType={"google"}
+            onClick={signInWithGoogle}
+            type="button"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignInForm;
---
+++ capstone/crown-clothing/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,81 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqxrRAEPCuhCrAR-KkPMXxhCvjHd2xaHM",
+  authDomain: "crwn-clothing-db-ddb92.firebaseapp.com",
+  projectId: "crwn-clothing-db-ddb92",
+  storageBucket: "crwn-clothing-db-ddb92.appspot.com",
+  messagingSenderId: "1032539974042",
+  appId: "1:1032539974042:web:3fb86cab90cd0b536fb223",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log("error creating use", error.message);
+    }
+  }
+
+  return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
+};
