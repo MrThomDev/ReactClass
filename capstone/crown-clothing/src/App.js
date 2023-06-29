3b941b5f3e2c365c2125c18a8cb32ac817d64914
Thomas Ryan <DWTrip@gmail.com>
2023-06-29 15:17:00 +0900

    adding cart context, shopping icon, and cart dropdown.

--- capstone/crown-clothing/src/App.js
+++ capstone/crown-clothing/src/App.js
@@ -1,16 +1,8 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
-
-const Shop = () => {
-  return (
-    <div>
-      <h1>I am the shop page</h1>
-      <Outlet />
-    </div>
-  );
-};
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
   return (
--- capstone/crown-clothing/src/routes/navigation/navigation.component.jsx
+++ capstone/crown-clothing/src/routes/navigation/navigation.component.jsx
@@ -3,12 +3,18 @@ import { Outlet, Link } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import "./navigation.styles.scss";
 
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import { UserContext } from "../../contexts/user.context";
+
+import { CartContext } from "../../contexts/cart.context";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
   return (
     <Fragment>
       <div className="navigation">
@@ -28,7 +34,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon></CartIcon>
         </div>
+        {isCartOpen && <CartDropdown></CartDropdown>}
       </div>
       <Outlet />
     </Fragment>
